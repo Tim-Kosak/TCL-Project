127fc84cd7c88581180856894d00739bb126b0c2
Tim KOSAK <tim.kosak@etu.univ-lyon1.fr>
2021-09-23 21:11:08 +0200

    Add research the nearest bus stop from GPS point feature

---
+++ main/tools.py
@@ -0,0 +1,20 @@
+import math
+from .models import *
+
+def dist(P1, P2):
+    return math.sqrt((P1[0] - P2[0])**2 + (P1[1] - P2[1])**2)
+
+def get_nearset_stop(stop_list, orig_lat, orig_lon):
+    res_id = 0
+    min_dist = dist([orig_lat, orig_lon], [stop_list[0].coord1, stop_list[0].coord2])
+
+    
+    for s in stop_list:
+        distance = dist([orig_lat, orig_lon], [s.coord1, s.coord2])
+
+        if min_dist > distance:
+            res_id = s.id
+            min_dist = distance
+            print("Nouvel arrêt trouvé : " + s.nom)
+
+    return res_id
--- main/views.py
+++ main/views.py
@@ -1,10 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from .tools import *
+from decimal import Decimal
 
 # Create your views here.
 
 def index(request):
-    stop_list = Stop.objects.all()
+    stop_list = Stop.objects.filter(coord1__lte = 1000, coord2__lte = 1000) #Filtre sur les adresses non valides
 
-    return render(request, 'index.html', {"stop_list" : stop_list} )
+    lat = Decimal(request.GET.get("lat"))
+    lon = Decimal(request.GET.get("lon"))
+
+    id_res = get_nearset_stop(stop_list, lat, lon)
+
+    context = {
+        "stop_list" : stop_list,
+        "lat" : lat,
+        "lon" : lon,
+        "res_size" : len(stop_list),
+        "nearest_stop" : Stop.objects.get(id=id_res)
+    }
+    return render(request, 'index.html', context )
